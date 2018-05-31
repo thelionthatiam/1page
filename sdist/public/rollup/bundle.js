f3408f38fdfd710dd4fe63fca75cf4218b13eb6c
Julian Theberge <juliantheberge@gmail.com>
2018-05-30 21:33:01 -0700

    fixed compile error

--- sdist/public/rollup/bundle.js
+++ sdist/public/rollup/bundle.js
@@ -20624,6 +20624,18 @@ var a = (function (exports) {
 	function PhotoIcon(props) {
 	    return (react_4("svg", {className: props.styles, id: "Layer_1", "data-name": "Layer 1", xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 479.04 415.75"}, react_4("title", null, "photos-icon"), react_4("polyline", {className: 'aline', points: "7.94 204.88 240.52 340.7 473.1 204.88"}), react_4("polyline", {points: "7.94 275.88 240.52 411.7 473.1 275.88"}), react_4("polygon", {points: "6.94 139.88 239.52 4.05 472.1 139.88 239.52 275.7 6.94 139.88"})));
 	}
+	// export function PhotoIcon(props) {
+	//     return (
+	//         <svg className={props.styles}
+	//             id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400"
+	//         >
+	//             <title>photos-icon</title>
+	//             <polyline className='aline' points="7.94 204.88 240.52 340.7 473.1 204.88" />
+	//             <polyline points="7.94 275.88 240.52 411.7 473.1 275.88" />
+	//             <polygon points="6.94 139.88 239.52 4.05 472.1 139.88 239.52 275.7 6.94 139.88" />
+	//         </svg>
+	//     )
+	// }
 	function X(props) {
 	    return (react_4("svg", {className: props.styles, viewBox: "0 0 20 20", version: "1.1", xmlns: "http://www.w3.org/2000/svg", onClick: props.onClick, onMouseOver: props.onMouseOver, onMouseLeave: props.onMouseLeave}, react_4("line", {x1: "1", y1: "11", x2: "11", y2: "1"}), react_4("line", {x1: "1", y1: "1", x2: "11", y2: "11"})));
 	}
--- src/public/svg/icons.tsx
+++ src/public/svg/icons.tsx
@@ -68,19 +68,19 @@ export function CurlIcon(props) {
     )
 }
 
-export function PhotoIcon(props) {
+// export function PhotoIcon(props) {
 
-    return (
-        <svg className={props.styles}
-            id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400"
-        >
-            <title>photos-icon</title>
-            <polyline className='aline' points="7.94 204.88 240.52 340.7 473.1 204.88" />
-            <polyline points="7.94 275.88 240.52 411.7 473.1 275.88" />
-            <polygon points="6.94 139.88 239.52 4.05 472.1 139.88 239.52 275.7 6.94 139.88" />
-        </svg>
-    )
-}
+//     return (
+//         <svg className={props.styles}
+//             id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400"
+//         >
+//             <title>photos-icon</title>
+//             <polyline className='aline' points="7.94 204.88 240.52 340.7 473.1 204.88" />
+//             <polyline points="7.94 275.88 240.52 411.7 473.1 275.88" />
+//             <polygon points="6.94 139.88 239.52 4.05 472.1 139.88 239.52 275.7 6.94 139.88" />
+//         </svg>
+//     )
+// }
 
 export function X(props) {
     return (
